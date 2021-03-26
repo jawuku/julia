2ad43cfe8d8970753a0514978b3103000f7e26fa
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2021-03-26 06:35:49 +0000

    Update and rename 02Dot-product-commutative.jl to 02DotProductCommutative.jl

--- linear-algebra-code-challenges/02DotProductCommutative.jl
+++ linear-algebra-code-challenges/02DotProductCommutative.jl
@@ -1,5 +1,11 @@
-# Dot product commutative?
+#= udemy.com code challenge
+from Prof. Mike X Cohen's course in Linear Algebra
+https://www.udemy.com/course/linear-algebra-theory-and-implementation
+
+# Is the dot product commutative?
 # A.B == B.A ?
+=#
+
 using LinearAlgebra
 
 # generate two 100-element random row vectors, transposed from default column layout.
@@ -21,13 +27,13 @@ dp2 = dot(b, a)
 
 # display them
 println("Dot product of a and b is $(dp1)")
-println("Dot product of b and a is $(dp2)")
+println("Dot product of b and a is $(dp2)\n")
 
 # are they equal?
 if dp1 == dp2
-  println("Therefore, dot product of the 100-element vector is commutative.")
+  println("The dot product of the 100-element vector is commutative.\n")
 else
-  println("Dot product is not commutative")
+  println("Dot product is not commutative.\n")
 end
 
 # generate 1 row, 2 column array with random integers from -10:10
@@ -36,7 +42,7 @@ c = rand(-10:10, 1, 2)
 d = rand(-10:10, 1, 2)
 
 println("Array c = $c")
-println("Array d = $d")
+println("Array d = $d\n")
 
 dp3 = dot(c, d)
 dp4 = dot(d, c)
@@ -45,7 +51,7 @@ println("Dot product of c and d is $(dp3)")
 println("Dot product of d and c is $(dp4)")
 
 if dp3 == dp4
-  println("Thus, dot product of the 2-element vector is commutative.")
+  println("Thus, the dot product of the 2-element vector is commutative.")
 else
   println("Dot product is not commutative")
 end
