c02cf339ab9dbe222c78e4ec023fed0a921e3d40
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2021-04-10 03:59:13 +0100

    should eliminate BoundsError

--- linear-algebra-code-challenges/03Matrix_Multiply.jl
+++ linear-algebra-code-challenges/03Matrix_Multiply.jl
@@ -1,28 +1,27 @@
 ## Multiply 2 matrices
 # checks for invalid dimensions
 # uses 1st method (element dot product method)
-# WIP - get BoundsError on some combinations
-
 function matmul(A,B)
 
+# convert any 1D column vectors into 1*x 2D matrices
+  if ndims(A) = 1
+    A = A[:,:]
+  end
+  
+  if ndims(B) = 1
+    B = B[:,:]
+  end
+  
 # define row and column sizes
 # for each matrix
-  julia
-
+  rowA, colA = size(A)
+  rowB, colB = size(B)
+  
 # check that dimensions are valid for multiplication, otherwise throw error
   if colA != rowB
-    throw(DimensionMismatch("Matrices of dimensions $(colA) and $(rowB) cannot be multiplied together"))
+    throw(DimensionMismatch("Matrices of dimensions $(size(A)) and $(size(B)) cannot be multiplied together"))
   end
 
-# convert any 1D column vectors into 1*x 2D matrices
-  if   colA == 1
-    A = reshape(A, length(A), 1)
-  end
-  
-  if colB == 1
-    B = reshape(B, length(B), 1)
-  end
-  
 # initialise zero matrix to hold result
 # matrix size is the outer dimensions of the two matrices
   result = zeros(rowA,colB)
@@ -41,4 +40,3 @@ end
 # return the result
   return result
 end
-
