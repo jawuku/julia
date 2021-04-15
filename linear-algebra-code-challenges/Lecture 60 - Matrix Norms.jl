48bc1ec31d004e8cb4f83dc4f57562ccb31a3f70
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2021-04-15 15:06:18 +0100

    Update Lecture 60 - Matrix Norms.jl

--- linear-algebra-code-challenges/Lecture 60 - Matrix Norms.jl
+++ linear-algebra-code-challenges/Lecture 60 - Matrix Norms.jl
@@ -14,7 +14,7 @@ add them together
 take square root
 
 =#
-
+A = [1 2 3;4 5 6;7 8 9]
 # naive implementation
 frobnorm1(A) = sqrt(reduce(+, (map(x -> x^2, A))))
 
@@ -27,7 +27,7 @@ using LinearAlgebra
 
 frobnorm2(A) = sqrt(tr(A' * A))
 
-frobnorm3(A) = norm(A)
+frobnorm3(A) = norm(A) # default in Julia
 #=
 
 2) Induced 2-norm - ascertains how much matrix A scales vector x
@@ -42,6 +42,8 @@ divided by the magnitude of vector x
 
 twonorm(A,x) = norm(A * x) / norm(x)
 
+normInd2(A) = sqrt(eigmax(A' * A)) 
+
 # if A is a pure rotation matrix, then ||Ax|| would be equal to ||x||
 # so the induced two-norm would be 1
 
@@ -55,9 +57,24 @@ Singular Value Decomposition determines these values
 
 =#
 
-# Examples
+p = 1 # Schatten 1-norm
+s = svdvals(A) # extracts singular values into a vector
+normSchat = sum(s.^p)^(1/p)
 
-A = [1 2 3;
-     4 5 6;
-     7 8 9]
+p = 2 # same as Frobenius norm
+normSchat = sum(s.^p)^(1/p)
 
+#= Orthogonal Matrix
+   -----------------
+
+All the columns have a magnitude of 1, and each column is par-wise orthogonal to every other column
+
+This can be a purely rotational matrix, so the induced 2-norm is 1
+
+Can be extracted as the Q matrix of the pair from QR Decomposition
+=#
+
+Q, R = qr(randn(5,5))
+
+# induced 2-norm = 1
+ind2norm = sqrt(eigmax(Q' * Q))
